2e9ac3d3e7a3573c5203c68657b2e827fadb4daf
evsasse <evsasse@gmail.com>
2015-06-16 17:13:40 -0300

    Inherited GUI, so objects in it can be called, added a example behavior to the button

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog
-from main_ui import Ui_Form  # here you need to correct the names
+from main_ui import Ui_Form
+
+def printHam():
+	print("Banana")
+
+class MainInit(QDialog):
+	def __init__(self):
+		super(MainInit, self).__init__()
+
+		# Set up the user interface from Designer.
+		self.ui = Ui_Form()
+		self.ui.setupUi(self)
+
+		# Connect up the buttons.
+		self.ui.regex_to_fa_btn.clicked.connect(printHam)
 
 app = QApplication(sys.argv)
-window = QDialog()
-ui = Ui_Form()
-ui.setupUi(window)
-
+window = MainInit()
 window.show()
 sys.exit(app.exec_())
